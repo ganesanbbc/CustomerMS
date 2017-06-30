9bc569dd9af8b3b644957e4084a03a5c776f4884
Navaneethan <navaneethan.t@cognizant.com>
2017-06-30 11:05:54 +0530

    Changed the DAO for get customers

--- src/main/java/com/cts/customer/dao/CustomerDao.java
+++ src/main/java/com/cts/customer/dao/CustomerDao.java
@@ -31,23 +31,9 @@ public class CustomerDao {
     Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
     KeyFactory keyFactory = datastore.newKeyFactory().setKind(TABLE_NAME);
    
-   
-
 
     public Customer createCustomer(Customer customer){
-//    	 DatastoreOptions options = null;
-//		try {
-//			options = DatastoreOptions.newBuilder()
-//				  .setProjectId("customerms")
-//				  .setCredentials(GoogleCredentials.fromStream(
-//				    new FileInputStream(("./customermssample-bc2c739383f6.json")))).build();
-//		} catch (FileNotFoundException e) {
-//			e.printStackTrace();
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//		}
-//       		Datastore datastore = options.getService();
-//       	 KeyFactory keyFactory = datastore.newKeyFactory().setKind(TABLE_NAME);
+
         Key key = datastore.allocateId(keyFactory.newKey());
         Entity entity = Entity.newBuilder(key).set("Customer_Name",customer.getUserName())
                 .set("Customer_Email",customer.getMailId()).set("Customer_Location",customer.getLocation())
@@ -59,19 +45,7 @@ public class CustomerDao {
     }
 
     public List<Customer> getAllCustomers(){
-//    	 DatastoreOptions options = null;
-// 		try {
-// 			options = DatastoreOptions.newBuilder()
-// 				  .setProjectId("customerms")
-// 				  .setCredentials(GoogleCredentials.fromStream(
-// 				    new FileInputStream(("./customermssample-bc2c739383f6.json")))).build();
-// 		} catch (FileNotFoundException e) {
-// 			e.printStackTrace();
-// 		} catch (IOException e) {
-// 			e.printStackTrace();
-// 		}
-//        		Datastore datastore = options.getService();
-//        	 KeyFactory keyFactory = datastore.newKeyFactory().setKind(TABLE_NAME);
+
         try {
             log.warning("Calling getAllCustomers");
             ArrayList<Customer> customerList = new ArrayList();
@@ -98,31 +72,15 @@ public class CustomerDao {
             }
             return customerList;
         } catch(Exception ex){
-        	ex.printStackTrace();
             log.warning("Exception : "+ex.getMessage());
             return  null;
         }
     }
     
     public Customer getCustomersById(long  customerId){
-//    	 DatastoreOptions options = null;
-// 		try {
-// 			options = DatastoreOptions.newBuilder()
-// 				  .setProjectId("customerms")
-// 				  .setCredentials(GoogleCredentials.fromStream(
-// 				    new FileInputStream(("./customermssample-bc2c739383f6.json")))).build();
-// 		} catch (FileNotFoundException e) {
-// 			e.printStackTrace();
-// 		} catch (IOException e) {
-// 			e.printStackTrace();
-// 		}
-//        		Datastore datastore = options.getService();
-//        	 KeyFactory keyFactory = datastore.newKeyFactory().setKind(TABLE_NAME);
         try {
             log.warning("Calling getAllCustomers");
             Customer customerNew = new Customer();
-//            Filter propertyFilter =
-//            	    new Filter("height", FilterOperator.GREATER_THAN_OR_EQUAL, minHeight);
             Query<Entity> entityQuery = Query.newEntityQueryBuilder().setKind(TABLE_NAME).
             		setFilter(PropertyFilter. eq("Customer_Id",""+customerId)).build();
 
@@ -133,39 +91,21 @@ public class CustomerDao {
                 	customerNew = new Customer(customer.getKey().getId(), customer.getString("Customer_Name"),
                             customer.getString("Customer_Email"), customer.getString("Customer_Location"),
                             "");
-//                    customerList.add(tempCustomer);
                 }else{
                     log.info("Customer Key Id is NULL ");
                 }
             }
             return customerNew;
         } catch(Exception ex){
-        	 ex.printStackTrace();
             log.warning("Exception : "+ex.getMessage()+ ex.getMessage());
-           
             return  null;
         }
     }
     
     public List<Customer> searchCustomersByName(String  customerName){
-//    	 DatastoreOptions options = null;
-// 		try {
-// 			options = DatastoreOptions.newBuilder()
-// 				  .setProjectId("customerms")
-// 				  .setCredentials(GoogleCredentials.fromStream(
-// 				    new FileInputStream(("./customermssample-bc2c739383f6.json")))).build();
-// 		} catch (FileNotFoundException e) {
-// 			e.printStackTrace();
-// 		} catch (IOException e) {
-// 			e.printStackTrace();
-// 		}
-//        		Datastore datastore = options.getService();
-//        	 KeyFactory keyFactory = datastore.newKeyFactory().setKind(TABLE_NAME);
         try {
             log.warning("Calling searchCustomersByName"+customerName);
             ArrayList<Customer> customerList = new ArrayList();
-//            Filter propertyFilter =
-//            	    new Filter("height", FilterOperator.GREATER_THAN_OR_EQUAL, minHeight);
             Query<Entity> entityQuery = Query.newEntityQueryBuilder().setKind(TABLE_NAME).build();
 
             Iterator<Entity> entityIterator = datastore.run(entityQuery);
