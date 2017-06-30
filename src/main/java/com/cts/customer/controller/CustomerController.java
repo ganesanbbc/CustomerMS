f2ab5c0c7e9dff1054b26a31d3b13240710c5eee
Navaneethan <navaneethan.t@cognizant.com>
2017-06-30 11:53:46 +0530

    Added search functionality in index

--- src/main/java/com/cts/customer/controller/CustomerController.java
+++ src/main/java/com/cts/customer/controller/CustomerController.java
@@ -13,6 +13,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -52,17 +53,17 @@ public class CustomerController {
                 "service", new ServiceDetails(returnCustomer.getId()));
     }
 
-    @RequestMapping(value = CustomerEndPoints.SEARCH_CUSTOMER_URL, method = RequestMethod.GET)
-    public String searchCustomerGet(Model model) {
-        List<Customer> customerList = customerService.getAllCustomers();
-        model.addAttribute(CUSTOMERS_ATTR, customerList);
-        model.addAttribute(SEARCH_ATTR, "");
-        return CustomerEndPoints.SEARCH_CUSTOMER_URL;
-    }
+//    @RequestMapping(value = CustomerEndPoints.SEARCH_CUSTOMER_URL, method = RequestMethod.GET)
+//    public String searchCustomerGet(Model model) {
+//        List<Customer> customerList = customerService.getAllCustomers();
+//        model.addAttribute(CUSTOMERS_ATTR, customerList);
+//        model.addAttribute(SEARCH_ATTR, "");
+//        return CustomerEndPoints.SEARCH_CUSTOMER_URL;
+//    }
 
 
     @RequestMapping(value = CustomerEndPoints.SEARCH_CUSTOMER_URL, method = RequestMethod.POST)
-    public String searchCustomer(String search, Model model) {
+    public String searchCustomer(@RequestParam String search, Model model) {
         List<Customer> customerList = null;
         if (search != null && !search.isEmpty()) {
             customerList = customerService.searchCustomersByName(search);
@@ -70,7 +71,7 @@ public class CustomerController {
         }
         model.addAttribute(CUSTOMERS_ATTR, customerList);
         model.addAttribute(SEARCH_ATTR, search);
-        return CustomerEndPoints.SEARCH_CUSTOMER_URL;
+        return CustomerEndPoints.INDEX_URL;
     }
 
     @RequestMapping(value = CustomerEndPoints.VIEW_CUSTOMER_URL, method = RequestMethod.GET)
